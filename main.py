3275442964f7b9be7f28a45d8fe4896b10a1bc95
Nandy Bâ <nandyba@yahoo.fr>
2020-01-05 23:43:04 +0100

    Fixes of inconsistent use of tabs and spaces in indentation line 44 of main.py file

--- main.py
+++ main.py
@@ -15,23 +15,23 @@ text_regex_list = ["txt", "doc", "docx", "odt", "pdf", "tex", "epub"]
 Files_moved = []
 
 def main():
-    print("Downloads folder organizer")
-    patterns = "*"
-    ignore_patterns = ""
-    ignore_directories = False
-    case_sensitive = True
-    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
-    my_event_handler.on_created = on_created
-    go_recursively = True
-    my_observer = Observer()
-    my_observer.schedule(my_event_handler, track_dir)
-    my_observer.start()
-    try:
-         while True:
-             time.sleep(1)
-    except KeyboardInterrupt:
-         my_observer.stop()
-         my_observer.join()
+	print("Downloads folder organizer")
+	patterns = "*"
+	ignore_patterns = ""
+	ignore_directories = False
+	case_sensitive = True
+	my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+	my_event_handler.on_created = on_created
+	go_recursively = True
+	my_observer = Observer()
+	my_observer.schedule(my_event_handler, track_dir)
+	my_observer.start()
+	try:
+		 while True:
+			 time.sleep(1)
+	except KeyboardInterrupt:
+		 my_observer.stop()
+		 my_observer.join()
 
 def sort_file(file):
 	global Files_moved
@@ -41,9 +41,9 @@ def sort_file(file):
 	file_extension = filename.rsplit('.', 1)[1]
 	if(not(filename in Files_moved) and file_extension != 'tmp' ):
 		print(filename + " has been detected")
-        while(os.path.isfile(file) == False):
-            time.sleep(1)
-            
+		while(os.path.isfile(file) == False):
+			time.sleep(1)
+			
 		if(file_extension in audio_regex_list):
 			move_file(filename, path, cfg.path['music'])
 		elif(file_extension in text_regex_list):
@@ -51,8 +51,8 @@ def sort_file(file):
 	
 
 def on_created(event):
-    file = event.src_path
-    sort_file(file)
+	file = event.src_path
+	sort_file(file)
 
 def move_file(filename, oldPath, newPath):
 	global Files_moved
